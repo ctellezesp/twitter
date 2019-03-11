5f973bd4bc2c9f1446be28873397aa43c3bd56a4
ctellezesp <a01274912@itesm.mx>
2019-03-11 13:20:25 -0600

    landing page added

--- src/index.js
+++ src/index.js
@@ -41,7 +41,22 @@ class Twitter extends React.Component{
 						</div>
 					</div>
 					<div className="tweets">
-
+						<div className="the-tweet">
+								<div className="img-tweet">
+									<img className="img-profile" src="http://www.femalefirst.co.uk/image-library/square/500/1/1024x5347096774193549origin0x11cristianoronaldo.jpg" />
+								</div>
+							<div className="body-tweet">
+								<div className="header-tweet">
+									<span className="username">@cristiano</span>
+									<span className="date">11-03-2019</span>
+								</div>
+								<div className="content-tweet">
+									<span className="text-tweet">
+										There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+									</span>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
