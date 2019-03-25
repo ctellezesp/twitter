9f8bdd27ba8921b6ff4b6da8c7a3dd0e89b66829
ctellezesp <a01274912@itesm.mx>
2019-03-25 08:33:29 -0600

    tweet fetch added

--- src/index.js
+++ src/index.js
@@ -17,32 +17,117 @@ class Twitter extends React.Component{
 		this.myRef = React.createRef();
 		this.handleClick = this.handleClick.bind(this);
 		this.state = {
-			tweets: []
+			tweets: [],
+			error: null,
+			isLoaded: false
 		}
 	}
 
+	componentDidMount(){
+		fetch("http://10.24.50.178:3001/tweets")
+		.then(res => res.json())
+		.then(
+			(result) => {
+				this.setState({
+					isLoaded: true,
+					tweets: result.tweets
+				})
+			},
+			(error) => {
+				this.setState({
+					isLoaded: true,
+					error: error
+				})
+			}
+		)
+	}
+
 	handleClick(){
-		this.setState({tweets: [...this.state.tweets, {user: '@cristiano', date: new Date().toString(), post: this.myRef.current.value}]});
+		/*this.setState({tweets: [...this.state.tweets, {user: '@cristiano', date: new Date().toString(), post: this.myRef.current.value}]});
 		console.log(this.state.tweets);
 		this.myRef.current.value = '';
         this.myRef.current.focus();
+        */
+        this.setState({
+        	isLoaded: false
+        });
+        let newTweet = {
+        	user_name: 'Carlos',
+        	avatar: 'https://avatars0.githubusercontent.com/u/26472750?s=460&v=4',
+        	tweet_content: this.myRef.current.value
+        }
+
+        let headers = {};
+        headers['Content-Type'] = 'application/json';
+
+        const options = {
+        	headers: headers,
+        	method: 'POST',
+        	body: JSON.stringify(newTweet)
+        }
+
+        fetch("http://10.24.50.178:3001/tweets", options)
+        .then(res => res.json())
+        .then(
+        	(result) =>{
+        		let newTweet = result.tweet;
+        		let tweets = this.state.tweets.slice();
+        		this.setState({
+        			isLoaded: true,
+        			tweets: tweets.concat(newTweet)
+        		})
+        	},
+        	(error) => {
+        		this.setState({
+        			isLoaded: false,
+        			error: error
+        		})
+        	}
+        )
+
+
 	}
 
 
 	render(){
+		const {error, isLoaded, tweets} = this.state;
+		let content;
+		if(error){
+			content = <div>No hay Tweets</div>;
+		}
+		else{
+			content = tweets.map((tweet) => 
+				<div className="the-tweet" key={tweet.id}>
+					<div className="img-tweet">
+						<img className="img-profile" src={tweet.avatar} />
+					</div>
+					<div className="body-tweet">
+						<div className="header-tweet">
+							<span className="username">{tweet.user_name}</span>
+							<span className="date">{tweet.created_at}</span>
+						</div>
+						<div className="content-tweet">
+							<span className="text-tweet">
+								{tweet.tweet_content}
+							</span>
+						</div>
+					</div>
+				</div>
+			);
+		}
 		return(
 			<div id="main">
 				<div className="profile">
-					<img className="img-profile" src="http://www.femalefirst.co.uk/image-library/square/500/1/1024x5347096774193549origin0x11cristianoronaldo.jpg" />
+					<img className="img-profile" src="https://avatars0.githubusercontent.com/u/26472750?s=460&v=4" />
 					<div className="profile-info">
-						<span className="username">@cristiano</span>
-						<span className="name">Cristiano Ronaldo</span>
+						<span className="username">@ctellezesp</span>
+						<span className="name">Carlos</span>
 					</div>
 				</div>
 				<div className="landing">
 					<div className="bar">
 						<div className="img-bar">
-							<img className="img-profile" src="http://www.femalefirst.co.uk/image-library/square/500/1/1024x5347096774193549origin0x11cristianoronaldo.jpg" />
+							<img className="img-profile" src="https://avatars0.githubusercontent.com/u/26472750?s=460&v=4" />
 						</div>
 						<div className="myTweet">
 							<textarea className="post-tweet" type="text" placeholder="What's Happening?" ref={this.myRef}></textarea>
@@ -52,24 +137,8 @@ class Twitter extends React.Component{
 						</div>
 					</div>
 					<div className="tweets">
-						{this.state.tweets.map((tweet, key) => 
-							<div className="the-tweet" key={key}>
-								<div className="img-tweet">
-									<img className="img-profile" src="http://www.femalefirst.co.uk/image-library/square/500/1/1024x5347096774193549origin0x11cristianoronaldo.jpg" />
-								</div>
-								<div className="body-tweet">
-									<div className="header-tweet">
-										<span className="username">{tweet.user}</span>
-										<span className="date">{tweet.date}</span>
-									</div>
-									<div className="content-tweet">
-										<span className="text-tweet">
-											{tweet.post}
-										</span>
-									</div>
-								</div>
-							</div>
-						)}
+						{content}
+						
 					</div>
 				</div>
 			</div>
